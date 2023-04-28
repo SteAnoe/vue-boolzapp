a38614b6f892c0362042d29b2c0b628d933f12dc
SteAnoe <ste.anoe@gmail.com>
2023-04-28 10:21:27 +0200

    bonus menu tendina + currentdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,17 +15,20 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     }
                 ],
             },
@@ -37,17 +40,20 @@ createApp({
                     {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     }
                 ],
             },
@@ -59,17 +65,20 @@ createApp({
                     {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     }
                 ],
             },
@@ -81,12 +90,14 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     }
                 ],
             },
@@ -98,12 +109,14 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     }
                 ],
             },
@@ -115,17 +128,20 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     }
                 ],
             },
@@ -137,12 +153,14 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     }
                 ],
             },
@@ -154,17 +172,20 @@ createApp({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                        status: 'sent'
+                        status: 'sent',
+                        display: false
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
-                        status: 'received'
+                        status: 'received',
+                        display: false
                     }
                 ],
             }
@@ -181,23 +202,38 @@ createApp({
     addMessage(){
         if(this.valueInput != ''){
             let sentMessage = {
-                date: '10/01/2020 15:30:55',
+                date: this.currentDate(),
                 message: this.valueInput,
-                status: 'sent'
+                status: 'sent',
+                display: false
             }
             this.contacts[this.contact].messages.push(sentMessage)
             this.valueInput = '' 
             setTimeout( ()=> { 
                 let receivedMessage = {
-                    date: '10/01/2020 15:30:55',
+                    date: this.currentDate(),
                     message: 'ok',
-                    status: 'received'
+                    status: 'received',
+                    display: false
                 }
                 this.contacts[this.contact].messages.push(receivedMessage)
                 this.valueInput = ''
             }, 1000 )  
         }
     },
+    changeDisplay(i){
+        if(i.display == false){
+            i.display = true
+        } else{
+            i.display = false
+        }
+    },
+    deleteMessage(index){
+        this.contacts[this.contact].messages.splice(index, 1 )
+    },
+    currentDate: function(){
+        return dayjs().format('DD/MM/YYYY HH:mm:ss');
+    }
   }
 }).mount('#app')
 
